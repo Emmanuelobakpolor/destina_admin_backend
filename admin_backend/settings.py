c2f516c83936371bea84bbef4fcba17c18bf8e61
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-10-17 23:26:24 +0100

    b

--- admin_backend/settings.py
+++ admin_backend/settings.py
@@ -31,7 +31,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4je_*^e8geokjq8#)(503
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS.append('destina-admin-backend.onrender.com')
 
 
 REST_FRAMEWORK = {
@@ -45,6 +50,10 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'users.User'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://destina-admin-backend.onrender.com',
+]
 
 # Application definition
 
