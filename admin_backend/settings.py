a4ea48627e75fe69183ab6f594624fa3a596898d
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-10-18 11:37:15 +0100

    b

--- admin_backend/settings.py
+++ admin_backend/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ✅ ADD THIS
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
